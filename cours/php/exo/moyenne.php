fd09367e4b804d70d9cd1e6f2bc563d90a767d50
laurent <pro@laurent-chaloupe.fr>
2022-11-09 16:32:36 +0100

    Boucle : pour faire tant que tant que faire

--- cours/php/exo/moyenne.php
+++ cours/php/exo/moyenne.php
@@ -23,3 +23,44 @@ for ($i = 0; $i < $nbrNote; $i++) {
 
 $moyenne = $somme / $nbrNote;
 echo 'Moyenne = ' . $moyenne . '<br>';
+
+echo '<br>';
+$somme = 0;
+$nbrNote = 2;
+$i = 0;
+
+//for ($i = 0; $i < $nbrNote; $i++) {
+do {
+    $i++;
+
+    $note = rand(0, 10);
+    echo 'Note = ' . $note . '<br>';
+
+    $somme = $somme + $note;
+
+    echo 'Somme = ' . $somme . '<br>';
+} while($i < $nbrNote);
+
+$moyenne = $somme / $nbrNote;
+echo 'Moyenne = ' . $moyenne . '<br>';
+
+
+echo '<br>';
+$somme = 0;
+$nbrNote = 2;
+$i = 0;
+
+//for ($i = 0; $i < $nbrNote; $i++) {
+while($i < $nbrNote) {
+    $i++;
+
+    $note = rand(0, 10);
+    echo 'Note = ' . $note . '<br>';
+
+    $somme = $somme + $note;
+
+    echo 'Somme = ' . $somme . '<br>';
+};
+
+$moyenne = $somme / $nbrNote;
+echo 'Moyenne = ' . $moyenne . '<br>';
