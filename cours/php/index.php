859b72ff41e937c9aea0e38d6e47ef978d5f6fa1
laurent <pro@laurent-chaloupe.fr>
2022-11-10 10:08:49 +0100

    cours sur les fonctions

--- cours/php/index.php
+++ cours/php/index.php
@@ -51,3 +51,74 @@ while($count < 10) {
     echo 'count = '.$count.'<br>';
     $count++;
 }
+
+test('laurent');
+
+function  test(string $nom) : void
+{
+    echo ucfirst($nom).'<br>';
+}
+
+$i = 0;
+$i = $i + 1; // est l'équivalent de $i++;
+$i++;
+$chaineDeCaractere = 'Bonjour';
+$chaineDeCaractere = $chaineDeCaractere . ' Guillaume'; // est l'équivalent de $chaineDeCaractere .= ' Guillaume';
+$chaineDeCaractere .= ' Guillaume';
+
+
+$monNom = "teissier";
+$monPrenom = "thomas";
+
+echo nomPrenom($monNom, $monPrenom);
+echo nomPrenom("Grolier", "anthony");
+
+function nomPrenom(string $nom, string $prenom) : string
+{
+    return "Mon nom est : ".ucfirst($nom)."<br>Mon prénom est : ".ucfirst($prenom).'<br>';
+}
+
+$maBoissonEst = 'pisse mémé';
+boire($maBoissonEst);
+boire('bière');
+boire('café');
+boire($maBoissonEst);
+$laurent = 10;
+echo maNote('laurent', $laurent);
+$thomas = 15;
+echo maNote('thomas', $thomas);
+$anthony = 20;
+$anthonyPrenom = 'Anthony';
+echo maNote($anthonyPrenom, $anthony);
+echo maNote('yann', 21);
+
+function boire(string $boisson) : void
+{
+    echo 'Tu bois (du/de la): '.ucfirst($boisson).'<br>';
+}
+
+/**
+ * Appreciation sur une note donnée
+ * 
+ * @param string $prenom prénom
+ * @param int $note      Note de l'éleve
+ * @return string
+ */
+function maNote(string $prenom, int $note) : string
+{
+    $resultatDeLaNote = 'La note de '.ucfirst($prenom).' est '.$note.' <br>';
+
+    if ($note > 20) {
+        $resultatDeLaNote .= "Arrête de boire ou double les doses";
+    } else {
+        if ($note >= 12 && $note <= 20) {
+            $resultatDeLaNote .= "excellent";
+        } elseif (11 == $note) {
+            $resultatDeLaNote .= "moyen";
+        } else {
+            $resultatDeLaNote .= "Réessaye encore";
+        }
+    }
+
+    return $resultatDeLaNote.'<br>';
+}
