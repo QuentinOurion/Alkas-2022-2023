82588351162ee11d834f023e2e2583e6497bb85d
laurent <pro@laurent-chaloupe.fr>
2022-11-10 16:26:08 +0100

    rassemble tous les exos sur la même page

--- cours/php/exo/tousLesExos.php
+++ cours/php/exo/tousLesExos.php
@@ -2,16 +2,43 @@
 /**
  * Page de tous les exos PHP
  */
+$laurentPrenom = 'laurent';
 $laurentNote = 10;
-echo maNote('laurent', $laurentNote);
+echo appreciationNote($laurentPrenom, $laurentNote);
 $thomasNote = 15;
-echo maNote('thomas', $thomasNote);
+$thomasPrenom = 'thOmas';
+echo appreciationNote($thomasPrenom, $thomasNote);
 $anthonyNote = 20;
 $anthonyPrenom = 'Anthony';
-echo maNote($anthonyPrenom, $anthonyNote);
-$yannAppreciation = maNote('yann', 21);
+echo appreciationNote($anthonyPrenom, $anthonyNote);
+$yannAppreciation = appreciationNote('yann', 21);
 echo $yannAppreciation;
 
+//$laurentNombreNote = 12;
+//$laurentSommeNote = sommeNotes($laurentNombreNote);
+//$laurentMoyenne = moyenneNotes($laurentSommeNote, $laurentNombreNote);
+//echo "Laurent somme des notes : $laurentSommeNote, la moyenne est : $laurentMoyenne<br>";
+
+echo evaluationEleve($anthonyPrenom, 10);
+echo evaluationEleve($thomasPrenom, 22);
+echo evaluationEleve($laurentPrenom, 5);
+
+//Laurent somme des notes : 161, la moyenne est : 13.416666666667
+/**
+ * Evaluation d'un eleve
+ *
+ * @param string $prenom
+ * @param int $nombreNote
+ * @return string        Phrase formatée de l'évaluation
+ */
+function evaluationEleve(string $prenom, int $nombreNote): string
+{
+    $somme = sommeNotes($nombreNote);
+    $moyenne = moyenneNotes($somme, $nombreNote);
+    $prenom = ucfirst(strtolower($prenom));
+
+    return "<strong>$prenom</strong> a un nombre de notes : $nombreNote et la somme de ses notes est = $somme. La moyenne de $prenom est : $moyenne<br>";
+}
 
 /**
  * Appreciation sur une note donnée
@@ -20,7 +47,7 @@ echo $yannAppreciation;
  * @param int $note      Note de l'éleve
  * @return string
  */
-function maNote(string $prenom, int $note) : string
+function appreciationNote(string $prenom, int $note) : string
 {
     $resultatDeLaNote = 'La note de '.ucfirst($prenom).' est '.$note.' <br>';
 
@@ -37,4 +64,37 @@ function maNote(string $prenom, int $note) : string
     }
 
     return $resultatDeLaNote.'<br>';
+}
+
+/**
+ * Calcul la somme des notes d'un éleve
+ *
+ * @param int $nombreNote
+ * @return int Somme des notes
+ */
+function sommeNotes(int $nombreNote): int
+{
+    $somme = 0;
+
+    for($i = 0; $i < $nombreNote; $i++) {
+        $note = rand(0, 21);
+
+//        echo "note = $note<br>";
+        $somme = $somme + $note;
+    }
+
+    return $somme;
+}
+
+/**
+ * Calcul de la moyenne des notes
+ *
+ * @param int $somme
+ * @param int $nombreNote
+ * @return float Moyenne des notes
+ */
+function moyenneNotes(int $somme, int $nombreNote) : float
+{
+    $moyenne = $somme / $nombreNote;
+    return $moyenne;
 }
