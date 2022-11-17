46c369116da2a77ed576942196851dff52ef43a7
laurent <pro@laurent-chaloupe.fr>
2022-11-17 15:42:07 +0100

    autres

--- cours/php/htmlPhp/header.php
+++ cours/php/htmlPhp/header.php
@@ -1,1 +1,12 @@
-<header role="banner"><h1><?=$titre?></h1></header>
+<header role="banner"><h1><?= $titre ?></h1></header>
+<ul>
+<?php
+$continent = '';
+foreach ($countries_list as $list) {
+    if ($continent != $list['continent']) {
+        $continent = $list['continent'];
+        echo $continent . '<br>';
+    }
+}
+?>
+</ul>
