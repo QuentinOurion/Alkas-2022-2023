2303b3fd1eb7a10c70a8558a18f4d6d3e1c6731e
laurent <pro@laurent-chaloupe.fr>
2022-11-21 15:24:38 +0100

    correction partielle de l'exo calculatrice.php

---
+++ cours/php/exo/calculatrice.php
@@ -0,0 +1,57 @@
+<?php
+require_once ('fonctions/outils.php');
+
+$nombre1 = 0;
+$operateur = '';
+$nombre2 = 0;
+$historique = 'histo';
+
+if(!empty($_POST['operateur'])) {
+    $operateur = $_POST['operateur'];
+}
+
+if(!empty($_POST['nombre1'])) {
+    $nombre1 = $_POST['nombre1'];
+}
+
+if(!empty($_POST['nombre2'])) {
+    $nombre2 = $_POST['nombre2'];
+}
+// code identique ci-dessous
+//if(isset($_POST['nombre2']) && $_POST['nombre2']) {
+//    $nombre2 = $_POST['nombre2'];
+//}
+
+if(is_numeric($nombre1) && $operateur && is_numeric($nombre2)) {
+    pre($_POST);
+/*    if('+' == $operateur) {
+        echo $nombre1 + $nombre2;
+    } elseif('-' == $operateur) {
+        echo $nombre1 - $nombre2;
+    }elseif('*' == $operateur) {
+        echo $nombre1 * $nombre2;
+    }elseif('/' == $operateur) {
+        echo $nombre1 / $nombre2;
+    } else {
+        echo 'Gros con';
+    }*/
+    
+    switch ($operateur) {
+        case '+':
+            $resultat = $nombre1 + $nombre2;
+            break;
+        case '-':
+            $resultat = $nombre1 - $nombre2;
+            break;
+        case '*':
+            $resultat = $nombre1 * $nombre2;
+            break;
+        case '/':
+            $resultat = $nombre1 / $nombre2;
+            break;
+        default:
+            $resultat = 'Gros con !';
+    }
+}
+
+require_once ('template/calculatrice.php');
