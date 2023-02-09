0b5b34f19e7f26623117e74becb18b946aea0ab8
Laurent <pro@laurent-chaloupe.fr>
2023-02-09 16:30:19 +0100

    formulaire

--- cours/php/symfony/intitule.md
+++ cours/php/symfony/intitule.md
@@ -3,4 +3,4 @@
 ## First symf
 Créez une card en bootsrap avec le titre qui devra être dynamique et son contenu aussi
 
-Vous devez pour ça créer un nouveau controlleur qui s'appelle "Exo" avec une route qui devra s'appeler "firstExo"
+Vous devez pour ça créer un nouveau contrôleur qui s'appelle "Exo" avec une route qui devra s'appeler "firstExo"
--- cours/php/symfony/src/Controller/AlkasController.php
+++ cours/php/symfony/src/Controller/AlkasController.php
@@ -3,16 +3,44 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AlkasController extends AbstractController
 {
-    #[Route('/shifumi', name: 'shifumi')]
+    #[Route('/', name: 'pageIndex')]
     public function index(): Response
     {
         return $this->render('alkas/index.html.twig', [
             'macle' => 'mon test',
         ]);
     }
+
+    #[Route('/response', name: 'pageIndex')]
+    public function response(): Response
+    {
+        return new Response('test', 404);
+    }
+
+    #[Route('/formulaire', name: 'formulaire')]
+    public function formulaire(Request $request): Response
+    {
+        $premierChamp = 'vide';
+
+        if($request->request->count()) {
+
+            if($request->request->get('premierChamp')) {
+                $premierChamp = $request->request->get('premierChamp') . ' pas vide';
+            }
+        }
+
+//        dd());
+        return $this->render('alkas/formulaire.twig', [
+            'premierChamp' => $premierChamp,
+            'nombre' => $request->request->count()
+        ]);
+    }
+
+
 }
